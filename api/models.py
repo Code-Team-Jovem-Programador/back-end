35099c80d2907b7dc075d114e920b03a087c7be0
davidm07 <0794david@gmail.com>
2024-11-04 20:36:08 -0300

    id uuid feito

--- api/models.py
+++ api/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class Produto(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     quantidade = models.IntegerField()
