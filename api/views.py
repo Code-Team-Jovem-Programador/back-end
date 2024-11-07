14e014bd4e962247eeb2e9e3f6bba7419e551be7
Matheus Menger <109791126+M-Menger@users.noreply.github.com>
2024-11-06 21:37:03 -0300

    Criada Tabela no SupaBase
    Criada tabela do banco de dados no supabase

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -18,14 +19,25 @@ from . import serializers
                      
 @api_view(['GET', 'POST'])
 def get_produtos(request):
+=======
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from . import models
+# from . import serializers
 
-    if request.method == 'GET':
-        produtos = models.Produto.objects.all()
-        serializer = serializers.ProdutoSerializer(produtos, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':  
-        serializer = serializers.ProdutoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# # Create your views here.
+# @api_view(['GET', 'POST'])
+# def get_produtos(request):
+>>>>>>> Stashed changes
+
+#     if request.method == 'GET':
+#         produtos = models.Produto.objects.all()
+#         serializer = serializers.ProdutoSerializer(produtos, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':  
+#         serializer = serializers.ProdutoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
