d70c17670ae4723c823a59ccbe6621fcc38aa2e1
Matheus Menger <matheus-menger@hotmail.com>
2024-11-08 20:45:52 -0300

    Adicionado rota para download do relatório em excel

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('produtos/', views.get_produtos),
+    path('produtos/', views.get_produtos),
+    path('export/', views.produtos_report_xlsx, name='produtos_report_xlsx'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from . import models
 from drf_yasg.utils import swagger_auto_schema
+from django.http import HttpResponse
+from openpyxl import Workbook
 from . import serializers
+from . import models
+import csv
 
 # Create your views here.
 @swagger_auto_schema(
@@ -28,3 +31,30 @@ def get_produtos(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def produtos_report_xlsx(request):
+    produtos = models.Produto.objects.all()
+
+    workbook = Workbook()
+    worksheet = workbook.active
+    worksheet.title = 'Produtos'
+
+    worksheet.append(['ID', 'PRODUTO', 'DESCRICAO', 'QUANTIDADE', 'PRECO', 'CATEGORIA'])
+
+    for produto in produtos:
+        worksheet.append([str(produto.id),
+                          produto.nome,
+                          produto.descricao,
+                          produto.quantidades,
+                          produto.preco,
+                          produto.categoria])
+        
+        response = HttpResponse(
+            content_type='application/vnd.opexmlformats-officedocument.spreadsheetml.sheet'
+        )
+
+        response['Content-Disposition'] = 'attachment; filename="Produtos.xlsx"'
+        workbook.save(response)
+
+        return response
