7eccc8c77dd0023dcf77cd327fccc79fd5384d31
davidm07 <0794david@gmail.com>
2024-11-11 20:57:27 -0300

    admin att

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from api import models
+
 # Register your models here.
+@admin.register(models.Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'quantidades', 'preco', 'categoria')
+    list_display_links = ('nome', 'descricao', 'quantidades', 'preco', 'categoria')
