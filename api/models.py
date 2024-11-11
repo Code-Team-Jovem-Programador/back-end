2091e09f9e4b007cceeb2b4154e9eaa500b90ece
davidm07 <0794david@gmail.com>
2024-11-11 19:47:29 -0300

    price att

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     quantidades = models.IntegerField()
-    preco = models.FloatField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.CharField(max_length=255)
 
     def __str__(self):
