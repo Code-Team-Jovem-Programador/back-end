0fca57c34b913d69992efbbf258d0b615ec22f8e
Matheus Menger <matheus-menger@hotmail.com>
2024-12-04 00:34:58 -0300

    Funcionando o envio de confirmação de cadastro para o email

--- config/settings.py
+++ config/settings.py
@@ -111,8 +111,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Para Gmail, por exemplo
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'code.team.senac@gmail.com'
-EMAIL_HOST_PASSWORD = '@Codeteam2024'
+EMAIL_HOST_PASSWORD = 'qgfb xkfi dxmk tmhc'
 DEFAULT_FROM_EMAIL = 'code.team.senac@gmail.com'
 
 # Database
